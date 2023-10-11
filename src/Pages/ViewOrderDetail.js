6ca443231e9502663417a06d8b74159ade297dc2
fasih <fasih0070@gmail.com>
2023-10-11 07:39:41 +0500

    .

--- src/Pages/ViewOrderDetail.js
+++ src/Pages/ViewOrderDetail.js
@@ -3,14 +3,35 @@ import { useParams } from "react-router-dom";
 import { useFirebase } from "../context/Firebase";
 
 function ViewOrderDetail() {
-  const [order, setOrders] = useState([]);
+  const [orders, setOrders] = useState([]);
   const firebase = useFirebase();
   const params = useParams();
-  console.log(params);
   useEffect(() => {
-    firebase.getOrders(params.bookId).then((res) => setOrders(res));
-  }, []);
-  return <div>ViewOrderDetail</div>;
+    firebase.getOrders(params.bookId).then((res) => setOrders(res.docs));
+  }, [firebase]);
+  return (
+    <div className="container mt-3">
+      <h1>Orders</h1>
+      {orders.map((order) => {
+        const data = order.data();
+        return (
+          <div
+            className="mt-5"
+            style={{ border: "1px solid", padding: "10px" }}
+            key={data.userID}
+          >
+            <h3>Order By : {data.userName}</h3>
+            <h4>Qunatity : {data.qty}</h4>
+            <p>Email : {data.userEmail}</p>
+          </div>
+        );
+      })}
+    </div>
+
+
+
+
+  );
 }
 
 export default ViewOrderDetail;
